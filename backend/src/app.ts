75d1545a35391583b63b18633a8eff872e5d698d
Emanuel Bernardi <emanuel.aobernardi@gmail.com>
2026-05-30 13:54:33 -0300

    feat: register error middleware in Express app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,14 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import heroRoutes from './routes/hero.routes';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/heroes', heroRoutes);
+
+app.use(errorMiddleware);
 
 export default app;
